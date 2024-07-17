2f5ed396015bdb340df92065a8dd625a0b8cc92d
wenlanc <wenlan@spots.edu>
2024-07-17 13:40:56 +0700

    add cridb map and opacity slider

--- app/GlobeComponent.tsx
+++ app/GlobeComponent.tsx
@@ -1,34 +1,67 @@
-import React from 'react';
-import { GlobeContextProvider, Globe, GeoVideo } from "@openglobus/openglobus-react";
-
+import React, { useState } from 'react';
+import { GlobeContextProvider, Globe } from "@openglobus/openglobus-react";
+import * as og from "@openglobus/og";
+import { Slider, TextField, Grid } from '@mui/material';
 import "@openglobus/og/css/og.css";
-import {Hurricane} from "next/dist/compiled/@next/font/dist/google";
+
+
 const GlobeComponent = () => {
+    const [ mapOpacity, setMapOpacity ] = useState(1.0);
+      
+    let bing = new og.Bing("OpenStreetMap");
+    var cridb = new og.layer.XYZ("CRIDBMap", {
+        url: "https://eureka.cridb.com/api/tiles/52505/equi/{z}/{x}/{y}.png",
+        opacity: mapOpacity,
+        isBaseLayer: false,
+        urlRewrite: function (s: { tileX: number; tileY: number; tileZoom: any; }, u: any) {
+            return og.utils.stringTemplate(u, {
+                x: s.tileX,
+                y: s.tileY + Math.pow(2, s.tileZoom - 2),
+                z: s.tileZoom
+            });
+        },
+    });
+
     return (
         <GlobeContextProvider>
-            <Globe>
-                <GeoVideo
-                    corners={[
-                        [
-                            -134.7904382939764,
-                            55.07955352950936
-                        ],
-                        [
-                            -54.984314759410594,
-                            54.98843914299802
-                        ],
-                        [
-                            -55.041854075913825,
-                            19.820153025849297
-                        ],
-                        [
-                            -134.89882012831265,
-                            19.631495126944017
-                        ]
-                    ]}
-                    name="test"
-                    src="https://openglobus.org/examples/examples/geoVideo/imergac_20160508_NASA.mp4"
-                />
+            <Grid
+                container
+                justify="space-around"
+                direction="row"
+            >
+                <Grid
+                    item
+                    md={3}
+                ></Grid>
+                <Grid
+                    item
+                    md={6}
+                >
+                    <Slider
+                        value={mapOpacity * 100}
+                        onChange={(e) => {
+                            setMapOpacity(Number(e.target.value /100.0));
+                            cridb.opacity = Number(e.target.value /100.0);
+                        }}
+                        color={'primary'}
+                    /> 
+                    <TextField
+                        name="Opacity"
+                        id="name"
+                        label="Opacity"
+                        fullWidth
+                        variant="outlined"
+                        margin="normal"
+                        type="number"
+                        value={mapOpacity}
+                    /> 
+                </Grid>
+            </Grid>    
+            <Globe
+                sunActive={false}
+                atmosphereEnabled={false}
+                layers={[bing, cridb]}
+                >  
             </Globe>
         </GlobeContextProvider>
     );
