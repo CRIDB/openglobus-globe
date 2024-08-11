4953b196c0ac444d0a0b7570a5041d4e960c2018
DESKTOP-VG9JTRI\wenlanc <wenlan@spots.edu>
2024-08-11 10:19:36 +0700

    update camera fly logic

--- app/Button.tsx
+++ app/Button.tsx
@@ -8,15 +8,15 @@ export const CustomButton = () => {
     const onClick = () => {
 
         if (globe){
-            // let destPos = new LonLat(10.13176, 46.18868, 10779);
-            // let viewPoi = new LonLat(9.98464, 46.06157, 3039);
+            let destPos = new LonLat(10.13176, 46.18868, 10779);
+            let viewPoi = new LonLat(9.98464, 46.06157, 3039);
             // let lookCart = globe.planet.ellipsoid.lonLatToCartesian(viewPoi);
             // let upVec = globe.planet.ellipsoid.lonLatToCartesian(destPos).normalize();
-            // // 0 - is an amplitude of the fly track
-            // globe.planet.camera.flyLonLat(destPos, lookCart, upVec, 0);
-            globe.planet.flyExtent(new Extent(new LonLat(9.53297, 46.02795), new LonLat(11.56212, 45.78692)));
+            // 0 - is an amplitude of the fly track
+            globe.planet.camera.flyLonLat(destPos);
+            //globe.planet.flyExtent(new Extent(new LonLat(9.53297, 46.02795), new LonLat(11.56212, 45.78692)));
         }
     }
 
-    return <Button onClick={onClick}>log globe</Button>
+    return <Button onClick={onClick}>Fly Camera</Button>
 }
